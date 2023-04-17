9c9db4d3e98e80f8521084677068c3a3a553a58e
Marc Delgado Bonet <madebo@uoc.edu>
2023-04-18 00:36:21 +0200

    Cambios de contenido + intento solución bug Netifly

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,1 +1,29 @@
-console.log('Hello world!');
+document.getElementById("mainMenuButton").addEventListener("click", function() {
+    let button = document.getElementById("langSelectorCheckbox");
+    button.checked = false;
+    button = document.getElementById("mainMenuCheckbox");
+    button.checked = !button.checked;
+});
+
+document.getElementById("langSelectorButton").addEventListener("click", function() {
+    let button = document.getElementById("mainMenuCheckbox");
+    button.checked = false;
+    button = document.getElementById("langSelectorCheckbox");
+    button.checked = !button.checked;
+});
+
+function titleSticky(){
+    const banner = document.querySelector('.banner');
+    const horizMenu = document.querySelector('.horizMenu');
+    while (banner.firstChild) {
+        horizMenu.appendChild(banner.firstChild);
+    }
+}
+
+function titleTop(){
+    const banner = document.querySelector('.banner');
+    const horizMenu = document.querySelector('.horizMenu');
+    while (horizMenu.firstChild) {
+        banner.appendChild(horizMenu.firstChild);
+    }
+}
